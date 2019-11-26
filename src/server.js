b3eba11de116c29c1390edeebf5e7e718007c764
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-11-26 17:14:27 -0300

    server configuration and routes definition

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const port = process.env.PORT;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-xxuxt.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => {
   console.log("Server running on port", port);
